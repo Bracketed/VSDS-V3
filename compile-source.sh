3c9be19cbe32f6bbff24829b5a0116c46ac958a5
ninjaninja140 <edenkneale@outlook.com>
2024-04-23 19:05:23 +0100

    testing major

---
+++ compile-source.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+source_path="./source/src"
+target_path="./Build/RuntimeTool"
+
+for dir in "$source_path"/*/; do
+    current_dir=${dir%/}
+
+    cp -r "$current_dir/"* "$target_path/"
+
+    lua_files=$(find "$target_path" -name "*.lua")
+
+    for lua_file in $lua_files; do
+        file_name=$(basename "$lua_file")
+
+        dotnet YolusCLI.dll "$file_name".lua
+
+        obfuscated_file="${target_path}/${file_name%.*}-obfuscated.lua"
+        echo "${file_name} was Obfuscated."
+
+        cp "$obfuscated_file" "$current_dir/"
+
+        rm "$lua_file"
+        rm "$obfuscated_file"
+    done
+done
