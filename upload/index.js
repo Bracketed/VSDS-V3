a543c7926c59505a2d281e312c556d82c85f5106
ninjaninja140 <edenkneale@outlook.com>
2024-04-29 08:09:05 +0100

    saddsaddaaasdsa

--- upload/index.js
+++ upload/index.js
@@ -2,13 +2,14 @@ const axios = require('axios');
 const process = require('node:process');
 const dotenv = require('dotenv');
 const fs = require('node:fs');
-const { hostname } = require('node:os');
 
 dotenv.config();
 
-async function uploadAsset(buffer, assetId) {
+async function uploadAsset() {
+	const buffer = Buffer.from(fs.readFileSync(`./${process.env.TARGETFILE}`));
+
 	const client = axios.create({
-		timeout: 60 * 3 * 1000, // 3 minutes timeout
+		timeout: 60 * 3 * 1000,
 		method: 'POST',
 		data: buffer,
 		headers: {
@@ -21,21 +22,20 @@ async function uploadAsset(buffer, assetId) {
 
 	console.debug('Uploading to Roblox...');
 	let response = await client
-		.request(`https://data.roblox.com/Data/Upload.ashx?assetid=${assetId}`)
+		.request(`https://data.roblox.com/Data/Upload.ashx?assetid=${process.env.TARGETASSET}`)
 		.then((d) => d)
 		.catch((e) => {
 			console.error(e);
 			return e;
 		});
 
-	// Check for CSRF challenge
 	if (response.response.status === 403 && response.response.headers['x-csrf-token']) {
 		const csrfToken = response.response.headers['x-csrf-token'];
 		console.debug('Received CSRF challenge, retrying with token...');
-		// Retry with CSRF token
+
 		client.defaults.headers.post['X-CSRF-Token'] = csrfToken;
 		response = await client
-			.request(`https://data.roblox.com/Data/Upload.ashx?assetid=${assetId}`)
+			.request(`https://data.roblox.com/Data/Upload.ashx?assetid=${process.env.TARGETASSET}`)
 			.then((d) => d)
 			.catch((e) => {
 				console.error(e);
@@ -43,12 +43,11 @@ async function uploadAsset(buffer, assetId) {
 			});
 	}
 
-	// Check if upload was successful
 	if (response.response.status >= 200 && response.response.status < 300) {
-		return; // Successful upload
+		return;
 	} else {
 		throw new Error(`Roblox API returned an error, status ${response.status}.`);
 	}
 }
 
-uploadAsset(Buffer.from(fs.readFileSync(`./${process.env.TARGETFILE}`)), process.env.TARGETASSET);
+uploadAsset();
