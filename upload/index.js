02b669e6069e19b4d54e00647b535341c10a114f
ninjaninja140 <edenkneale@outlook.com>
2024-04-30 08:18:09 +0100

    s

--- upload/index.js
+++ upload/index.js
@@ -6,12 +6,12 @@ const fs = require('node:fs');
 dotenv.config();
 
 async function uploadAsset() {
-	const buffer = fs.readFileSync(`./${process.env.TARGETFILE}`);
+	const buffer = fs.readFileSync(`./${process.env.TARGETFILE}`); // This is just the rbxm file
 
 	console.debug('Uploading to Roblox...');
 	let response = await axios
 		.post(`https://data.roblox.com/Data/Upload.ashx?assetid=${process.env.TARGETASSET}`, buffer, {
-			timeout: 60 * 3 * 1000,
+			timeout: 60 * 3 * 1000, // 3 mins
 			headers: {
 				Cookie: `.ROBLOSECURITY=${process.env.ROBLOXCOOKIE}`,
 				'User-Agent': 'Roblox/WinInet',
@@ -31,7 +31,7 @@ async function uploadAsset() {
 
 		response = await axios
 			.post(`https://data.roblox.com/Data/Upload.ashx?assetid=${process.env.TARGETASSET}`, buffer, {
-				timeout: 60 * 3 * 1000,
+				timeout: 60 * 3 * 1000, // 3 mins
 				headers: {
 					'X-CSRF-Token': csrfToken,
 					Cookie: `.ROBLOSECURITY=${process.env.ROBLOXCOOKIE}`,
@@ -50,7 +50,7 @@ async function uploadAsset() {
 	if (response.response.status >= 200 && response.response.status < 300) {
 		return;
 	} else {
-		throw new Error(`Roblox API returned an error, status ${response.status}.`);
+		throw new Error(`Unable to upload: ${response.status}`);
 	}
 }
 
