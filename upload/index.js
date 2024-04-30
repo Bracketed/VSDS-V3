51609aa77f17ac1f8b9f8f4ac33199b53d3695da
ninjaninja140 <edenkneale@outlook.com>
2024-04-30 18:26:40 +0100

    test

--- upload/index.js
+++ upload/index.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const process = require('node:process');
 const dotenv = require('dotenv');
 const fs = require('node:fs');
@@ -6,5 +5,25 @@ const nbx = require('noblox.js');
 
 dotenv.config();
 
-nbx.setCookie(process.env.ROBLOXCOOKIE);
-nbx.uploadModel(fs.readFileSync(`./${process.env.TARGETFILE}`), { name: 'VSDS-V3-DEV' }, process.env.TARGETASSET);
+async () => {
+	const usr = await nbx.setCookie(process.env.ROBLOXCOOKIE);
+	console.log(`Logged in as ${usr.UserName} [${usr.UserID}]`);
+
+	await nbx
+		.uploadModel(
+			fs.readFileSync(`./${process.env.TARGETFILE}`),
+			{
+				name: 'VSDS-V3-DEV',
+				description:
+					'The Virtua Source Deployment System development module, this is where our experimental code goes before going to the main VSDS loader.',
+				copyLocked: false,
+				allowComments: false,
+				groupId: 33845647,
+			},
+			process.env.TARGETASSET
+		)
+		.then(() => console.log('Uploaded new Version Successfully!'))
+		.catch((e) => {
+			throw new Error(e);
+		});
+};
