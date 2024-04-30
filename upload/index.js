cc158bc027a297ce5c96e76377d3c8686e9c4121
ninjaninja140 <edenkneale@outlook.com>
2024-04-30 18:32:28 +0100

    balls

--- upload/index.js
+++ upload/index.js
@@ -5,25 +5,20 @@ const nbx = require('noblox.js');
 
 dotenv.config();
 
-async () => {
-	const usr = await nbx.setCookie(process.env.ROBLOXCOOKIE);
-	console.log(`Logged in as ${usr.UserName} [${usr.UserID}]`);
+const usr = await nbx.setCookie(process.env.ROBLOXCOOKIE);
+console.log(`Logged in as ${usr.UserName} [${usr.UserID}]`);
 
-	await nbx
-		.uploadModel(
-			fs.readFileSync(`./${process.env.TARGETFILE}`),
-			{
-				name: 'VSDS-V3-DEV',
-				description:
-					'The Virtua Source Deployment System development module, this is where our experimental code goes before going to the main VSDS loader.',
-				copyLocked: false,
-				allowComments: false,
-				groupId: 33845647,
-			},
-			process.env.TARGETASSET
-		)
-		.then(() => console.log('Uploaded new Version Successfully!'))
-		.catch((e) => {
-			throw new Error(e);
-		});
-};
+const res = await nbx.uploadModel(
+	fs.readFileSync(`./${process.env.TARGETFILE}`),
+	{
+		name: 'VSDS-V3-DEV',
+		description:
+			'The Virtua Source Deployment System development module, this is where our experimental code goes before going to the main VSDS loader.',
+		copyLocked: false,
+		allowComments: false,
+		groupId: 33845647,
+	},
+	process.env.TARGETASSET
+);
+
+console.log(res);
