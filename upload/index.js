a877ce21dae25432ce9b38ca52d8d0ffb84c0811
ninjaninja140 <edenkneale@outlook.com>
2024-05-08 06:42:25 +0100

    test

--- upload/index.js
+++ upload/index.js
@@ -9,14 +9,6 @@ console.log(`Logged in as ${usr.UserName} [${usr.UserID}]`);
 
 const res = await nbx.uploadModel(
 	fs.readFileSync(`./vsds-${process.env.GITHUB_REF_NAME}.rbxm`),
-	{
-		name: 'VSDS-V3-DEV',
-		description:
-			'The Virtua Source Deployment System development module, this is where our experimental code goes before going to the main VSDS loader.',
-		copyLocked: false,
-		allowComments: false,
-		groupId: 33845647,
-	},
 	process.env.TARGETASSET
 );
 
