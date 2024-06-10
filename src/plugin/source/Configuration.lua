92c07ba106024053ea0244f6f75d7903942f15c0
ninjaninja140 <edenkneale@outlook.com>
2024-06-10 22:38:47 +0100

    s

--- src/plugin/source/Configuration.lua
+++ src/plugin/source/Configuration.lua
@@ -1,13 +1,11 @@
 local ASSETS = {}
 
--- // Library Storage
 ASSETS.Plugin = {}
 ASSETS.Services = {}
 ASSETS.Configuration = {}
 ASSETS.Version = "VERSION_UNKNOWN"
 ASSETS.Container = script:FindFirstAncestor('VSDS-PLUGIN')
 
--- // Default Built-ins for VSDS Assets
 ASSETS.Project = game
 ASSETS.Tick = tick()
 ASSETS.Require = require
@@ -16,7 +14,6 @@ ASSETS.String = string
 ASSETS.Math = math
 ASSETS.OS = os
 
--- // Plugin Config
 ASSETS.Configuration.ToolBarLogo =
     'https://www.roblox.com/asset/?id=17735487445'
 ASSETS.Configuration.ToolBarTitle = 'Plugins by Virtua.'
@@ -27,13 +24,11 @@ ASSETS.Configuration.ToolBarButton = {
     NAME = 'VSDS Importer Plugin'
 }
 
--- // Plugin Assets
 ASSETS.Plugin.Project = script:FindFirstAncestor('VSDS-PLUGIN')
 ASSETS.Plugin.Libraries = ASSETS.Plugin.Project['VSDS-Libraries']
 ASSETS.Plugin.Interface = ASSETS.Plugin.Project['VSDS-Interface']
 ASSETS.Plugin.Packages = ASSETS.Plugin.Project['VSDS-Packages']
 
--- // Plugin Services
 ASSETS.Services.RunService = ASSETS.Project:GetService('RunService')
 ASSETS.Services.HttpService = ASSETS.Project:GetService('HttpService')
 ASSETS.Services.ServerScriptService = ASSETS.Project:GetService(
