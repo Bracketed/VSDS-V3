d37c8532b5a7c38274b99e25a1466b767a673a3f
ninjaninja140 <edenkneale@outlook.com>
2024-06-12 17:11:46 +0100

    Updated Dictionary.lua

--- src/plugin/packages/Dictionary.lua
+++ src/plugin/packages/Dictionary.lua
@@ -1,24 +1,25 @@
 local None = newproxy(true)
 getmetatable(None).__tostring = function() return "None" end
 
-local function merge(...)
-    local output = {}
+return {
+    None = None,
+    merge = function(...)
+        local output = {}
 
-    for i = 1, select("#", ...) do
-        local source = select(i, ...)
+        for i = 1, select("#", ...) do
+            local source = select(i, ...)
 
-        if source ~= nil then
-            for key, value in pairs(source) do
-                if value == None then
-                    output[key] = nil
-                else
-                    output[key] = value
+            if source ~= nil then
+                for key, value in pairs(source) do
+                    if value == None then
+                        output[key] = nil
+                    else
+                        output[key] = value
+                    end
                 end
             end
         end
+
+        return output
     end
-
-    return output
-end
-
-return {None = None, merge = merge}
+}
